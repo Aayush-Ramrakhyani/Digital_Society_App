73d01c6ddbb3e5d0747b4e49818a251abd6df7a3
Aayush Ramrakhyani <143893649+Aayush-Ramrakhyani@users.noreply.github.com>
2024-06-28 13:32:47 +0530

    Delete lib directory

--- lib/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$host="localhost";
-$user="id22039557_root";
-$pw="Aayush@1225";
-$db="id22039557_digitalsociety";
-
-$con=mysqli_connect("localhost","id22039557_root","Aayush@1225","id22039557_digitalsociety") or die("disconnect");
-?>
